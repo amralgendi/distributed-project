638274410929e05f0d87b1043b3b70ed8008a871
Amr AlGendi <amrag201@gmail.com>
2024-05-20 01:01:30 +0300

    more host server

--- src/host_changer_server/server.py
+++ src/host_changer_server/server.py
@@ -12,7 +12,7 @@ def add():
     if ip == '': return json.dumps({})
 
     with open('/home/ubuntu/nodefile', 'a+') as file:
-        file.write(ip + " slots=16")
+        file.write(ip)
     with open('/home/ubuntu/host_num', 'r') as file:
         current_value = file.read().strip()
     with open('/home/ubuntu/host_num', 'w') as file:
@@ -25,8 +25,32 @@ def add():
 
 @app.route('/remove')
 def remove():
-    name = request.args.get('ip')  # 'name' is the query parameter
-    return f'Hello, {name}!'
+    ip = request.args.get('ip', '')  # 'name' is the query parameter
+    if ip == '': return json.dumps({})
+
+    with open('/home/ubuntu/nodefile', 'r+') as file:
+        existing_ips = file.read().splitlines()
+        if ip not in existing_ips:
+            print(f"IP address {ip} not found in the file.")
+            return
+        
+        # Reopen the file in write mode to clear it and rewrite the contents without the removed IP
+        file.seek(0)
+        file.truncate()
+        for ip in existing_ips:
+            if ip != ip:
+                file.write(ip + '\n')
+
+    with open('/home/ubuntu/host_num', 'r') as file:
+        current_value = file.read().strip()
+    with open('/home/ubuntu/host_num', 'w') as file:
+        file.write(str(int(current_value) - 1))
+
+    print(f"IP {ip} removed from the file.")
+
+    send_to_rmq(Event.REMOVE_NODE)
+
+    return json.dumps({"success": True})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080) # Running on http://0.0.0.0:8080
