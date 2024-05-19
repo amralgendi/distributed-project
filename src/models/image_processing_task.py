c4617d167c09d8f698959c33fcc7ed6af4a837b3
Amr AlGendi <amrag201@gmail.com>
2024-05-19 23:54:58 +0300

    model

--- src/models/image_processing_task.py
+++ src/models/image_processing_task.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import uuid
+from constants import PROCESSED_PATH
 
 # Define an enumeration for image processing operations
 class ImageModification(Enum):
@@ -9,11 +10,11 @@ class ImageModification(Enum):
 
 # Represents a job that applies effects to an image
 class ImageTask:
-    def __init__(self, mod_type: ImageModification, img_src: str):
-        self.task_id = str(uuid.uuid4()) # Generate a unique identifier
+    def __init__(self, id: str, img_path: str, mod_type: ImageModification):
+        self.task_id = id # Generate a unique identifier
         self.mod_type = mod_type # Store the type of image modification
-        self.img_src = img_src # Image source path
+        self.img_path = img_path
     
     def get_output_path(self):
         # Derive a path for saving processed images
-        return "output_images/" + self.task_id + ".png"
+        return PROCESSED_PATH + self.id + ".png"
