3fb23300c1334473783f663ea7088559a2f982d8
Amr AlGendi <amrag201@gmail.com>
2024-05-20 00:37:21 +0300

    typos

--- src/mpi/task_manager.py
+++ src/mpi/task_manager.py
@@ -40,12 +40,11 @@ class TaskManager():
             return
         if event == Event.START_PROCESSING:
             dataSplit = data.split(" ")
-            print(dataSplit)
             process_id = dataSplit[0]
             img_path = dataSplit[1]
             op_id = dataSplit[2]
 
-            data = " ".join([process_id, str(self.num_of_nodes)])
+            data = " ".join([process_id, str(self.num_of_nodes), op_id, img_path])
             send_to_rmq(Event.PROCESSING_STARTED, data)
 
             newThread = MPITask(self.num_of_nodes, process_id, img_path, op_id)
